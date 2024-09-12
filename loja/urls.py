1ad7f245c12477b1dde08462392e340fecb24ac5
egmoraes14 <egmoraes14@gmail.com>
2024-09-12 17:37:40 -0300

    criação primeira view

--- loja/urls.py
+++ loja/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from setup.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home' ),
+    path('home', home, name='home' ),
 ]
